07459ae9f3728a81f0b4eaac7ff4aed72aad1603
Luis <m.luishernandez@outlook.com>
2020-08-28 13:31:30 -0400

    First commit

---
+++ TaskTracker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TaskTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Story Number");
+            int storyNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Tittle");
+            string Tittle = Console.ReadLine();
+
+            Console.WriteLine("Time");
+            double Time = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Track: " + storyNum  + " " + Tittle + " " + Time);
+ 
+            
+        }
+    }
+}
